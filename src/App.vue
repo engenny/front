1ad232ac57b2ef2400e313294e81d6ac6db5390d
engenny <beatriz.gbds@gmail.com>
2021-10-20 11:29:37 -0300

    Change no projeto, utilizando js/html/css, ADD bootstrap no projeto

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@
     <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
+      <b-button>Button</b-button>
     </div>
     <router-view />
   </div>

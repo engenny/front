938dcd417bc9fa66113bcfea888aa4c722fc7687
engenny <beatriz.gbds@gmail.com>
2021-10-20 15:18:04 -0300

    ADD apechats

--- src/main.js
+++ src/main.js
@@ -5,14 +5,18 @@ import store from "./store";
 import { BootstrapVue, IconsPlugin } from "bootstrap-vue";
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap-vue/dist/bootstrap-vue.css";
+import VueApexCharts from "vue-apexcharts";
 
 
 Vue.config.productionTip = false;
 Vue.use(BootstrapVue);
 Vue.use(IconsPlugin);
+Vue.use(VueApexCharts);
 
 new Vue({
   router,
   store,
   render: (h) => h(App),
 }).$mount("#app");
+
+// Vue.component('apexchart', VueApexCharts)
